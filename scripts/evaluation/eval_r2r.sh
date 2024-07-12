d01186356a9cf73830779273563302b14fae405e
Sadanand Modak <sadanandm@utexas.edu>
2024-07-12 02:44:10 -0500

    tqdm progress bar not showing from navillm infer subprocess; so had to hack it

--- scripts/evaluation/eval_r2r.sh
+++ scripts/evaluation/eval_r2r.sh
@@ -9,10 +9,10 @@
 # activate environment
 # conda activate navillm
 
-torchrun --nnodes=1 --nproc_per_node=4 --master_port 41000 train.py \
+torchrun --nnodes=1 --nproc_per_node=8 --master_port 41000 train.py \
     --stage multi --mode test --data_dir data --cfg_file configs/multi.yaml \
     --pretrained_model_name_or_path data/models/Vicuna-7B --precision amp_bf16 \
     --resume_from_checkpoint data/model_with_pretrain.pt \
     --test_datasets R2R \
-    --jsonpath data/R2R/R2R_val_unseen_enc.json \
+    --jsonpath ../../tasks/R2R/data/R2R_val_unseen_enc.json \
     --batch_size 4 --output_dir build/eval --validation_split val_unseen --save_pred_results
