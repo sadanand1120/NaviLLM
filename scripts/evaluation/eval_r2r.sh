7541d2f7057f94b6b6db243ec19207beded23d2a
Sadanand Modak <sadanandm@utexas.edu>
2024-07-18 14:34:10 -0500

    DEBUGGING

--- scripts/evaluation/eval_r2r.sh
+++ scripts/evaluation/eval_r2r.sh
@@ -14,5 +14,5 @@ torchrun --nnodes=1 --nproc_per_node=8 --master_port 41000 train.py \
     --pretrained_model_name_or_path data/models/Vicuna-7B --precision amp_bf16 \
     --resume_from_checkpoint data/model_with_pretrain.pt \
     --test_datasets R2R \
-    --jsonpath ../../tasks/R2R/data/R2R_val_unseen_enc.json \
-    --batch_size 4 --output_dir build/eval --validation_split val_unseen --save_pred_results
+    --jsonpath ../../tasks/R2R/data/R2R_val_unseen_reduced_enc.json \
+    --batch_size 4 --output_dir build/eval --validation_split val_unseen_reduced --save_pred_results
